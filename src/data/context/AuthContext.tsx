00d789c315228ed74df8e6c7c70b6d07085d8fc9
Alessandro <silvaalessandro352@gmail.com>
2024-02-27 12:22:51 -0300

    teste

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,44 +1,44 @@
-import { createContext, useState } from 'react'
-import firebase from '../../firebase/config'
-import Usuario from '@/model/Usuario'
+// import { createContext, useState } from 'react'
+// import firebase from '../../firebase/config'
+// import Usuario from '@/model/Usuario'
 
-interface AuthContextProps{
-    usuario?: Usuario
-    loginGoogle: () => Promise<void>
-}
-
-const AuthContext = createContext<AuthContextProps>({})
-
-// async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
-//     const token = await usuarioFirebase.getIdToken()
-//     return {
-//         uid: usuarioFirebase.uid,
-//         nome: usuarioFirebase.displayName,
-//         email: usuarioFirebase.email,
-//         token,
-//         provedor: usuarioFirebase.providerData[0].providerId,
-//         imagemUrl: usuarioFirebase.photoURL
-// }
+// interface AuthContextProps{
+//     usuario?: Usuario
+//     loginGoogle: () => Promise<void>
 // }
 
-export function AuthProvider(props) {
+// const AuthContext = createContext<AuthContextProps>({})
 
-    const [usuario, setUsuario] = useState<Usuario>(null)
+// // async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
+// //     const token = await usuarioFirebase.getIdToken()
+// //     return {
+// //         uid: usuarioFirebase.uid,
+// //         nome: usuarioFirebase.displayName,
+// //         email: usuarioFirebase.email,
+// //         token,
+// //         provedor: usuarioFirebase.providerData[0].providerId,
+// //         imagemUrl: usuarioFirebase.photoURL
+// // }
+// // }
 
-    function loginGoogle() {
+// export function AuthProvider(props) {
 
-    }
+//     const [usuario, setUsuario] = useState<Usuario>(null)
 
-    return(
-    <AuthContext.Provider value={{
-        usuario,
-        loginGoogle
-    }}>
-        {props.children}
+//     function loginGoogle() {
 
-    </AuthContext.Provider>
-    )
-}
+//     }
+
+//     return(
+//     <AuthContext.Provider value={{
+//         usuario,
+//         loginGoogle
+//     }}>
+//         {props.children}
+
+//     </AuthContext.Provider>
+//     )
+// }
 
 
-export default AuthContext
+// export default AuthContext
